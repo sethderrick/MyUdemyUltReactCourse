6eff55e7d43c568169bbf18976fd6e561b879bb5
Seth Derrick <seth@sethderrick.tech>
2024-04-28 16:12:52 -0500

    Add ternaries section

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -178,3 +178,7 @@ const summary = `${title}, a ${pages}-page long book, was written by ${author} a
   publicationDate.split("-")[0]
 }.`;
 summary;
+
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages.`);
