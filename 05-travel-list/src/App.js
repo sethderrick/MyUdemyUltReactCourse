4dfcd1d6ce0ebf81c6fe1ee83c09a2c917a333eb
Seth Derrick <seth@sethderrick.tech>
2024-05-03 13:54:05 -0500

    Derived State

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handlToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -116,10 +116,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentPacked =
+    numItems > 0 ? Math.round((numPacked / numItems) * 100) : 0;
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list and you already packed X (X%)</em>
+      <em>
+        {percentPacked === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%)`}
+      </em>
     </footer>
   );
 }
