fa4cc076db3c54bb1480e3efa9abafcce0b326b5
Seth Derrick <seth@sethderrick.tech>
2024-07-02 17:32:23 -0500

    Section 16_188

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -1,34 +1,57 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
+
+function reducer(state, action) {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+
+    case "inc":
+      return { ...state, count: state.count + state.step };
+
+    case "setCount":
+      return { ...state, count: action.payload };
+
+    case "setStep":
+      return { ...state, step: action.payload };
+
+    case "reset":
+      return initialState;
+
+    default:
+      throw new Error("Uknown action");
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date();
   date.setDate(date.getDate() + count);
 
-  const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+  const dec = function() {
+    dispatch({ type: "dec" });
   };
 
-  const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+  const inc = function() {
+    dispatch({ type: "inc" });
   };
 
-  const defineCount = function (e) {
-    setCount(Number(e.target.value));
+  const defineCount = function(e) {
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
-  const defineStep = function (e) {
-    setStep(Number(e.target.value));
+  const defineStep = function(e) {
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
-  const reset = function () {
-    setCount(0);
-    setStep(1);
+  const reset = function() {
+    dispatch({ type: "reset" });
   };
 
   return (
